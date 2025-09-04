1585413fa1e890453ccba9b23b4c97cd1b45d11b
Prashant Vishwakarma <146864416+slayerboy228@users.noreply.github.com>
2025-09-04 16:34:37 +0530

    Update 216. Combination Sum III.cpp

--- 216. Combination Sum III.cpp
+++ 216. Combination Sum III.cpp
@@ -1,3 +1,4 @@
+// Solution 1
 class Solution {
     vector<vector<int>> result;
     
@@ -19,6 +20,33 @@ class Solution {
             t.pop_back();
         }
     }
+public:
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<int> t;
+        solve(1, k, n, t);
+        return result;
+    }
+};
+
+
+
+// Solution 2
+class Solution {
+    vector<vector<int>> result;
+    
+    void solve(int i, int k, int n, vector<int> &t) {
+        if (k == 0 && n == 0) {
+            result.push_back(t);
+            return;
+        }
+        if (i > 9 || n < 0 || k < 0) return;
+
+        t.push_back(i);
+        solve(i + 1, k - 1, n - i, t);
+        t.pop_back();
+        solve(i + 1, k, n, t);
+    }
+
 public:
     vector<vector<int>> combinationSum3(int k, int n) {
         vector<int> t;
