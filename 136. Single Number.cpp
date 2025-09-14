157bad5459e243c436fcab8535e55e24b452b99c
Prashant Vishwakarma <146864416+slayerboy228@users.noreply.github.com>
2025-09-14 20:54:21 +0530

    Update and rename Single Number (leetcode 136).cpp to 136. Single Number.cpp

--- 136. Single Number.cpp
+++ 136. Single Number.cpp
@@ -4,8 +4,7 @@ public:
     {
         int n = nums.size();
         int XOR = 0;
-        for (int i = 0; i < n; i++)
-        {
+        for (int i = 0; i < n; i++) {
             XOR = XOR ^ nums[i];
         }
         return XOR;
